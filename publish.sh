54227a6a906163c32a1c882bc053d1fee79955fd
Chien Kira <chienhedspi@gmail.com>
2019-02-18 22:09:46 +0900

    update publish script

--- publish.sh
+++ publish.sh
@@ -2,11 +2,11 @@
 
 DIR=$(dirname "$0")
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+#if [[ $(git status -s) ]]
+#then
+#    echo "The working directory is dirty. Please commit any pending changes."
+#    exit 1;
+#fi
 
 echo "Deleting old publication"
 rm -rf public
@@ -15,7 +15,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -24,4 +24,6 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
+
+cd ../
