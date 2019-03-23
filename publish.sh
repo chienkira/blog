6e56052f50c613778ce3468ed86b9ccd49f1d417
Chien Kira <chienhedspi@gmail.com>
2019-03-24 00:06:02 +0900

    update circle ci config

--- publish.sh
+++ publish.sh
@@ -24,6 +24,8 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
-
+cd public
+git add --all
+git commit -m "Publishing to gh-pages (publish.sh)"
+git push origin gh-pages
 cd ../
